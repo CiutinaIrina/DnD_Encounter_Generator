79ae0d8837894c7dc65eaa1a90dd1b7e54e7eb77
CiutinaIrina <aiciutina@gmail.com>
2025-12-03 16:41:31 +0200

    added popup

--- src/components/degPopup.vue
+++ src/components/degPopup.vue
@@ -1,13 +1,41 @@
 <script setup>
 
+import { ref, computed } from 'vue'
+
+const props = defineProps({
+  open: {
+    type: Boolean,
+    default: false,
+  },
+  anchorEl: {
+    type: Object,
+    default: () => null,
+  },
+})
+
+const positionStyles = computed(() => {
+  if (!props.anchorEl) return {};
+
+  const rect = props.anchorEl.$el.getBoundingClientRect();
+  return {
+    position: 'absolute',
+    top: `${rect.bottom + window.scrollY}px`,
+    left: `${rect.left + window.scrollX}px`,
+  };
+});
+
 </script>
 
 <template>
-  <div>
+  <div
+  v-if="open"
+  class="deg-popup"
+  :style="positionStyles"
+  >
       This is a popup
   </div>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "@/css/components/degPopup.scss";
 </style>
--- src/test/TestPopup.vue
+++ src/test/TestPopup.vue
@@ -1,14 +1,58 @@
 <script setup>
 import degPopup from '../components/degPopup.vue'
+import degIcon from '../components/degIcon.vue'
+import { ref } from 'vue'
 
+const isPopupOpen1 = ref(false)
+const isPopupOpen2 = ref(false)
+const isPopupOpen3 = ref(false)
+
+const onIconClick1 = () => {
+  isPopupOpen1.value = !isPopupOpen1.value
+}
+
+const onIconClick2 = () => {
+  isPopupOpen2.value = !isPopupOpen2.value
+}
+
+const onIconClick3 = () => {
+  isPopupOpen3.value = !isPopupOpen3.value
+}
 </script>
 
 <template>
   <div>
-    <deg-popup />
+    <deg-icon
+      ref="icon1"
+      name="save"
+      color="blue"
+      @click="onIconClick1"
+    />
+    <deg-popup
+      :open="isPopupOpen1"
+      :anchor-el="$refs.icon1"
+    />
+    <deg-icon
+      ref="icon2"
+      name="filter"
+      color="green"
+      @click="onIconClick2"
+    />
+    <deg-popup
+      :anchor-el="$refs.icon2"
+      :open="isPopupOpen2"
+    />
+    <deg-icon
+      ref="icon3"
+      name="info"
+      color="red"
+      @click="onIconClick3"
+    />
+    <deg-popup
+      :anchor-el="$refs.icon3"
+      :open="isPopupOpen3"
+    />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
